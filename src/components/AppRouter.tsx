5fdbee8c4d7700619686da10995156e073b07612
Valeria <lerik13@gmail.com>
2021-12-15 14:06:37 -0700

    architecture of project

---
+++ src/components/AppRouter.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { privateRoutes, publicRoutes, RouteNames } from '../router';
+import Login from '../pages/Login';
+import Event from '../pages/Event';
+
+const AppRouter = () => {
+	const auth = false;
+	
+	return (
+		auth ?
+			<Routes>
+				{privateRoutes.map(route => {
+					console.log(route);
+					 return <Route path={route.path} element={<Event />} key={route.path} /> 
+					/* return <Route path={route.path} element={route.element} /> */
+				}
+				)}
+				<Route path="/" element={<Navigate replace to={RouteNames.EVENT} />} />
+			</Routes>
+			:
+			<Routes>
+				{publicRoutes.map(route => 
+					<Route path={route.path} element={<Login />} key={route.path} />
+				)}
+				<Route path="/" element={<Navigate replace to={RouteNames.LOGIN} />} />
+			</Routes>
+	);
+};
+
+export default AppRouter;
